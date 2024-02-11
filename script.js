ab43be0a03a0d6267b090fb0e761c76cadd207db
vitomartino <vitor.martino2014@gmail.com>
2024-02-11 19:11:26 -0300

    Atualizações

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.getElementById('container');
 const inserirBtn = document.getElementById('inserir');
 const loginBtn = document.getElementById('login');
 const cancelarBtn = document.getElementById('cancelar');
+const prosseguirBtn = document.getElementById('prosseguir');
 
 inserirBtn.addEventListener('click', () => {
     container.classList.add("active");
